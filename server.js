e962a6706366e24879b54c47592e73de55a69f2b
eb110 <wfigura@op.pl>
2020-11-04 22:16:41 +0000

    klocki w tablicach

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const tetrisRouter = require('./routes/tetris')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
+
+
 app.use(express.static('public'))
 
 app.use('/', indexRouter)
+app.use('/tetris', tetrisRouter)
 
 app.listen(3000)
